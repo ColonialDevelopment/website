aac3af6ed5471439f1f9d7a677c87bf62397d0bd
DG Kim <donggyunkim@gmail.com>
2016-04-18 16:16:13 -0400

    Error messages

--- colonialsite/coloauth/urls.py
+++ colonialsite/coloauth/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'coloauth'
 urlpatterns = [
     url(r'^login', views.login_page, name='login_page'),
+    url(r'^logout', views.logout_page, name='logout_page'),
+    url(r'^register', views.register, name='register'),
 ]
--- colonialsite/coloauth/views.py
+++ colonialsite/coloauth/views.py
@@ -4,11 +4,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, redirect, render_to_response
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # login view
 def login_page(request):
     username = password = ''
+    if request.user.is_authenticated():
+        return HttpResponse('already logged in')
     if request.POST:
         username = request.POST['username']
         password = request.POST['password']
@@ -21,11 +24,33 @@ def login_page(request):
                 return HttpResponse('logged in')
             else:
                 # Return a 'disabled account' error message
-                return HttpResponse('disabled account')
+                template = loader.get_template('coloauth/login.html')
+                context = {'error_message': 'Account not activated'}
+                return HttpResponse(template.render(context, request))
         else:
             # Return an 'invalid login' error message.
-            return HttpResponse('wrong login')
+            template = loader.get_template('coloauth/login.html')
+            context = {'error_message': 'Invalid Password'}
+            return HttpResponse(template.render(context, request))
     else:
         template = loader.get_template('coloauth/login.html')
         context = {}
         return HttpResponse(template.render(context, request))
+    
+# logout view
+def logout_page(request):
+    logout(request)
+    return HttpResponse('logged out')
+
+# register view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/books/")
+    else:
+        form = UserCreationForm()
+    return render(request, "coloauth/register.html", {
+        'form': form,
+    })
