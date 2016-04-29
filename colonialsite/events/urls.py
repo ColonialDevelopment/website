8f886d1ae313680e0170fb0b72ecd3023abf4348
Cathy Chen <cathy.chen@princeton.edu>
2016-04-28 20:41:27 -0400

    events initial update
    still has exception 'The view events.views.create didn't return an
    HttpResponse object. It returned None instead.' for event/create

--- colonialsite/events/urls.py
+++ colonialsite/events/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
-app_name = 'events'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
+    url(r'^$', views.index, name='events_index'),
+    url(r'^create$', views.create, name = 'events_create'),
+    #ex. /events/view/1
+    url(r'^view/(?P<event_id>[0-9]+)$', views.view, name = 'events_view'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- colonialsite/events/views.py
+++ colonialsite/events/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import datetime
 
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.views.generic.dates import DayArchiveView, MonthArchiveView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+from .models import CreateForm, Event
 
-
-# Create your views here.
+@login_required
 def index(request):
-    return render(request, 'events/index.html')
+    return render(request, 'events/index.html', {'date': datetime.datetime.now()})
+
+@login_required
+def view(request, event_id):
+    try:
+        event = Event.objects.get(pk = event_id)
+    except Event.DoesNotExist:
+        raise Http404("Event does not exist.")
+
+    return render(request, 'events/view.html', {'event': event})
+
+@login_required
+def create(create):
+    if create.method == 'POST':
+        form = EventForm(event.POST)
+	if form.is_valid():
+	    new_event = form
+	    new_event.instance.submit_date = datetime.datetime.now()
+	    new_event.save()
+	    return HttpResponseRedirect('/events/view/' + new_event.instance.id.__str__())
+	else:
+	    form = CreateForm()
+        return render(request, "events/create.html", {'form': form, })
