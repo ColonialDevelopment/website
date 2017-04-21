b8451b88655a113a01ed27790998a935c20e4683
Nicholas Yang <nackster12@gmail.com>
2017-04-21 16:08:21 -0400

    Made some fields read-only in admin

--- colonialsite/members/admin.py
+++ colonialsite/members/admin.py
@@ -4,8 +4,10 @@ from .models import Member
 class MemberAdmin(admin.ModelAdmin):
     fields = ['name', 'netid', 'pref_name', 'officer_pos', 'birthday',
     		 'class_year', 'major', 'dorm', 'room_num', 'email',
-    		 'hometown', 'bio']
-    list_display = ['name', 'netid', 'class_year']
+    		 'hometown', 'bio',]
+    readonly_fields = ['name', 'netid', 'birthday', 'class_year', 'major',
+    					'dorm', 'room_num', 'email', 'hometown',]
+    list_display = ['name', 'netid', 'class_year',]
 
 admin.site.register(Member, MemberAdmin)
 
