e2738e7450127885f276e2206b09639eb1895272
Bryan Zhu <bwzhu@princeton.edu>
2017-04-22 13:34:17 -0400

    read members from csv

--- colonialsite/coloauth/usernames.py
+++ colonialsite/coloauth/usernames.py
@@ -1,17 +1,5 @@
-USER_LIST = [
-	'nyang',
-	'anniec',
-	'juliez', 
-	'junghwan',
-	'bwzhu', 
-	'ninaw',
-	'zyfan',
-	'ruchames',
-	'sjiao',
-	'mblucas',
-	'fukatsu',
-	'afuente',
-	'jcova',
-	'hshwang',
-	'tianayz',
-]
+import csv
+
+with open('./coloauth/members.csv', 'rb') as f:
+	reader = csv.reader(f, delimiter=',', quotechar='"')
+	USER_LIST = [row[1] for row in reader]
