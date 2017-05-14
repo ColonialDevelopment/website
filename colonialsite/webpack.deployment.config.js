a8276f8e2d47bb89be016721cb4a1b03a2a8c324
Andrew Ruchames <ruchames@princeton.edu>
2017-05-14 13:16:14 -0400

    Optimize deployment config for react

--- colonialsite/webpack.deployment.config.js
+++ colonialsite/webpack.deployment.config.js
@@ -17,8 +17,14 @@ module.exports = {
     },
 
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
         new BundleTracker({filename: './webpack-stats.json'}),
         new webpack.ProvidePlugin({
             $: 'jquery',
